eb1aeadc2aaa21c2292edf62aedd430b8bce61b9
amarshinde150 <amarshinde150@gmail.com>
2021-01-14 17:53:59 +0530

    Added some more functions

--- treeSort.c
+++ treeSort.c
@@ -1,4 +1,38 @@
 #include<stdio.h>
+struct tree
+{
+    int data;
+    struct tree *left,*right;
+} *head;
+
+void makeTree(int n,struct tree *root)
+{
+    struct tree *nn=(struct tree*)malloc(sizeof(struct tree));
+    nn->data=n;
+    nn->left=nn->right=NULL;
+    struct tree *temp=root;
+    if(root==NULL)
+    {
+        root=nn;
+    }
+    else if(temp->data>n)
+    {
+        temp=temp->left;
+        makeTree(n,temp);
+    }
+    else if(temp->data<n)
+    {
+        temp=temp->right;
+        makeTree(n,temp);
+    }
+}
+void treeSort(int *a,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        makeTree(a[i]);
+    }
+}
 int main()
 {
     int n;
